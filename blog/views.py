7949a5754882939055c65360e90c7137687dc8e8
trentalex <pars01@yandex.ru>
2020-04-21 22:50:32 +0300

    End base prodaction

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
 def post_list(request):
-    #posts = Post.objects.all()
-    posts = Post.objects.filter(status='published')
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    object_list = Post.objects.filter(status='published')
+    paginator = Paginator(object_list, 2)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
