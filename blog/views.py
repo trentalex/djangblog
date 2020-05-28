a3828b118b1f938718ed3a0886f9915aaac2416b
trentalex <pars01@yandex.ru>
2020-05-28 22:44:45 +0300

    Added apps blog and mysite

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
+from .forms import EmailPostForm
 
 def post_list(request):
     object_list = Post.objects.filter(status='published')
@@ -23,3 +24,17 @@ def post_detail(request, year, month, day, post):
         publish__day=day,
         slug=post,)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id,status='published')
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        # ... Отправка электронной почты.
+    else:
+        form = EmailPostForm()
+    return render(
+        request,
+        'blog/post/share.html',
+        {'post': post, 'form': form})
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from mysite import views
+
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),
 ]
